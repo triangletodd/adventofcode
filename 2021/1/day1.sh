0f6464410c66cb0ca072b4f992a7dbe4250d3ebc
Todd Edwards <github@rte.dev>
2021-12-01 11:00:07 -0500

    day1 part1

---
+++ 2021/1/day1.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -n $DEBUG ]]; then
+  set -x
+fi
+
+main() {
+  declare -i index=0
+  declare -i increases=0
+  declare -i previousindex=0
+
+  readarray -t input < ./input
+
+  for line in "${input[@]}"; do
+
+    if [[ $index -eq 0 ]]; then
+      let index=$index+1
+      continue
+    fi
+
+    previousindex=$(expr ${index}-1)
+
+    if [[ $line -gt ${input[$previousindex]} ]]; then
+      increases=$(expr ${increases}+1)
+    fi
+
+    index=$(expr ${index}+1)
+  done
+
+  echo $increases
+}
+
+main $@
